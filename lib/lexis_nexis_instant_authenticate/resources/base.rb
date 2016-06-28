fafa99bb92d97cb0105d2de2ce67888c9cafd4ff
Christopher Ostrowski <chris@madebyfunction.com>
2016-06-27 23:54:48 -0500

    Minor array ensure.

--- lib/lexis_nexis_instant_authenticate/resources/base.rb
+++ lib/lexis_nexis_instant_authenticate/resources/base.rb
@@ -31,7 +31,7 @@ module LexisNexisInstantAuthenticate
       end
 
       def h_dig(hash, *path)
-        path.inject(hash) do |location, key|
+        Array(path).inject(hash) do |location, key|
           location.respond_to?(:keys) ? location[key] : nil
         end
       end
