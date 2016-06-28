9a9150a2d187f95b2f2719a354c1b936a0eb5601
Christopher Ostrowski <chris@madebyfunction.com>
2016-06-28 00:19:53 -0500

    Fixed product responses.

--- lib/lexis_nexis_instant_authenticate/resources/quiz.rb
+++ lib/lexis_nexis_instant_authenticate/resources/quiz.rb
@@ -2,11 +2,11 @@ module LexisNexisInstantAuthenticate
   module Resources
     class Quiz < Base
       def questions
-        h_dig(proofing_response, :questions, :question)
+        h_dig(product_response, :questions, :question)
       end
 
       def id
-        h_dig(proofing_response, :questions, :"@ns:quiz_id")
+        h_dig(product_response, :questions, :"@ns:quiz_id")
       end
 
     end
--- lib/lexis_nexis_instant_authenticate/resources/score.rb
+++ lib/lexis_nexis_instant_authenticate/resources/score.rb
@@ -6,15 +6,15 @@ module LexisNexisInstantAuthenticate
       end
 
       def score
-        h_dig(proofing_response, :quiz_score)
+        h_dig(product_response, :quiz_score)
       end
 
       def quiz_id
-        h_dig(proofing_response, :quiz_score, :"@ns:quiz_id")
+        h_dig(product_response, :quiz_score, :"@ns:quiz_id")
       end
 
       def lex_id
-        h_dig(proofing_response, :"@ns:lex_id")
+        h_dig(product_response, :"@ns:lex_id")
       end
 
     end
